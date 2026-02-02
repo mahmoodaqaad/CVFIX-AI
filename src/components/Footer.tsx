a4bb77420559a28ea50d4b5d625ddb57fd6e1b8a
mahmood aqaad <mahmoodaqaad@gmail.com>
2026-02-02 14:36:37 +0200

    add portfolio

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export function Footer() {
     return (
         <footer className="border-t border-border bg-card mt-auto">
             <div className="container mx-auto px-4 py-6">
                 <p className="text-center text-sm text-muted-foreground">
-                    © {new Date().getFullYear()} CVFix AI. Built for Mahmood aqaad
+                    © {new Date().getFullYear()} CVFix AI. Built for <Link href={"https://my-portfolio-e3891.web.app/"} target="_blank" rel="noopener noreferrer">Mahmood Aqaad</Link>
                 </p>
             </div>
         </footer>
